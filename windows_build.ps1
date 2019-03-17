437ad8beaa35da1b8590c97d20718e1cfe29183e
Garai Ádám <garai.adam@marktsoft.hu>
2019-03-17 19:56:06 +0100

    windows build fixed with db init

--- windows_build.ps1
+++ windows_build.ps1
@@ -1,6 +1,5 @@
 $install_command = "npm install"
 $build_command = "npm run build"
-
 cd .\Backend 
 iex $install_command
 if ($?) {
@@ -9,28 +8,25 @@ if ($?) {
     echo "NPM INSTALL FAILED"
     exit -1
 }
-
 cd ..
 cd Frontend
-
 iex $install_command
-
 if ($?) {
     echo "NPM INSTALLED SUCCESFULLY"
 } else {
     echo "NPM INSTALL FAILED"
     exit -1
 }
-
 iex $build_command
-
 if ($?) {
     echo "NPM BUILDED SUCCESSFULLY"
 } else {
     echo "NPM BUILD FAILED"
     exit -1
 }
-
 cd ..
-
+$init_db = Read-Host "Do you want to init database? [Y/N]"
+if ($init_db -eq "Y") {
+    iex "powershell init_db.ps1"
+}
 echo "Everything is done"
