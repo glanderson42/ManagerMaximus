f4829386072b753ff34f42c06786439c56150fa8
glanderson42 <laveau.anderson42@protonmail.com>
2019-03-31 15:05:23 +0200

    created routing

--- Frontend/src/app/services/routing/app-routing.module.ts
+++ Frontend/src/app/services/routing/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../../sites/login/login.component';
 import { RegistrationComponent } from '../../sites/registration/registration.component';
+import { SuccessComponent } from '../../sites/emailconfirm/success/success.component';
+import { FailedComponent } from '../../sites/emailconfirm/failed/failed.component';
+import { WrongtokenComponent } from '../../sites/emailconfirm/wrongtoken/wrongtoken.component'
 
 const routes: Routes = [ {path: 'login', component: LoginComponent },
-                         {path: 'registration', component: RegistrationComponent } ];
+                         {path: 'registration', component: RegistrationComponent },
+                         {path: 'emailconfirm/success', component: SuccessComponent },
+                         {path: 'emailconfirm/failed', component: FailedComponent },
+                         {path: 'emailconfirm/wrongtoken', component: WrongtokenComponent } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
