ad2e5aebdc5d7c05a0811fbd219625866095425f
Attila Herczog <herczog.at97@gmail.com>
2019-04-10 15:08:29 +0200

    make delete request for project

--- Backend/src/models/project.js
+++ Backend/src/models/project.js
@@ -124,27 +124,50 @@ const del = (req, res) => {
     return;
   }
 
+  let data = {
+    statusCode: 403,
+    label: "Forbidden.",
+  }
   const projectId = database.escape(req.params.id);
-  database.asyncQuery("SELECT `project`.* FROM `project` LEFT JOIN `contributors` ON `project`.`id`=`contributors`.`projectid` WHERE (`contributors`.`userid`='" + loggedinUser.id + "' OR `project`.`authorid`='" + loggedinUser.id + "') AND `project`.`id` = " + projectId + ";")
-    .then(data=>{
-      // TODO: make DELETE
-      console.log(data)
-
-      res.status(200);
+  database.asyncQuery("DELETE FROM `project` WHERE `authorid`='" + loggedinUser.id + "' AND `id` = " + projectId + ";")
+    .then(result=>{
+      if(result.affectedRows > 0) {
+        data = {
+          statusCode: 200,
+          label: "Project delete was successful",
+        };
+        throw data;
+      } else {
+        return database.asyncQuery("DELETE FROM `contributors` WHERE `userid`='" + loggedinUser.id + "' AND `projectid`=" + projectId + ";");
+      }
+    })
+    .then(result=>{
+      if(result.affectedRows > 0) {
+        data = {
+          statusCode: 200,
+          label: "Contribution delete was successful",
+        }
+      }
+    })
+    .then(()=>{
+      res.status(data.statusCode);
       res.set({
         'Content-Type': 'application/json',
       })
-      res.send(JSON.stringify("{}"));
+      res.send(JSON.stringify(data));
     })
     .catch(err=>{
-      res.status(500);
+      if(err.sqlMessage) {
+        err = {
+          statusCode: 500,
+          label: err.sqlMessage,
+        }
+      }
+      res.status(err.statusCode);
       res.set({
         'Content-Type': 'application/json',
       })
-      res.send(JSON.stringify({
-        statusCode: 500,
-        label: err.sqlMessage,
-      }));
+      res.send(JSON.stringify(err));
     })
 }
 
