25357eded2115a1a1116b9d5fc0266fa19d74418
Naszradi Mihály <namisi@citromail.hu>
2019-05-01 15:04:39 +0200

    get projects with Authorization header

--- Frontend/src/app/services/auth/auth.service.ts
+++ Frontend/src/app/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { config } from '../../../../config/config';
 
 @Injectable()
@@ -18,5 +19,16 @@ export class AuthService {
 
   register(registrationData) {
     return this.httpClient.post(config.backendUrl + 'registration', registrationData);
+  }
+
+  getProjects() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const headers = new HttpHeaders(
+      {Authorization: 'Bearer ' + user.token}
+      );
+
+    return this.httpClient.get(config.backendUrl + 'projects/list', {
+      headers
+    });
   }
 }
