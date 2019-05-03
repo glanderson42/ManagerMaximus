dead9385da43f193e60070ea347eda7c611dc8a3
Attila Herczog <herczog.at97@gmail.com>
2019-05-03 14:39:23 +0200

    fix access control

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -43,8 +43,9 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
