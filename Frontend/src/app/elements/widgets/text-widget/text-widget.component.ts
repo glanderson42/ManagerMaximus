b10a15095e38a3f57922b603696d7dff7fe516be
Attila Herczog <herczog.at97@gmail.com>
2019-05-19 14:36:44 +0200

    add html to textwidget

--- Frontend/src/app/elements/widgets/text-widget/text-widget.component.ts
+++ Frontend/src/app/elements/widgets/text-widget/text-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-text-widget',
@@ -6,8 +6,22 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./text-widget.component.scss']
 })
 export class TextWidgetComponent implements OnInit {
+  @ViewChild('widgetContent') widgetContent: any;
 
-  @Input() widgetData;
+  get widgetData(){
+    return this.savedWidgetData;
+  }
+
+  @Input() set widgetData(set){
+    this.savedWidgetData = set;
+    this.convertedText = set.data;
+    this.convertedText = this.convertedText.replace(/[^"]((?:http|ftp|https):\/\/(?:[\w+?\.\w+])+(?:[a-zA-Z0-9\~\!\@\#\$\%\^\&\*\(\)_\-\=\+\\\/\?\.\:\;\'\,]*))[^"]/g, '<a href="$1" target="_blank">$1</a>');
+    this.convertedText = this.convertedText.replace(/\n/g, '<br>');
+    this.widgetContent.nativeElement.innerHTML = this.convertedText;
+  }
+
+  convertedText: String = "";
+  savedWidgetData: any;
 
   constructor() { }
 
