6cc2e148783a2fced39b2e6228d41d64e25f645c
Attila Herczog <herczog.at97@gmail.com>
2019-05-19 14:51:16 +0200

    url detector fix

--- Frontend/src/app/elements/widgets/text-widget/text-widget.component.ts
+++ Frontend/src/app/elements/widgets/text-widget/text-widget.component.ts
@@ -15,7 +15,7 @@ export class TextWidgetComponent implements OnInit {
   @Input() set widgetData(set){
     this.savedWidgetData = set;
     this.convertedText = set.data;
-    this.convertedText = this.convertedText.replace(/[^"]((?:http|ftp|https):\/\/(?:[\w+?\.\w+])+(?:[a-zA-Z0-9\~\!\@\#\$\%\^\&\*\(\)_\-\=\+\\\/\?\.\:\;\'\,]*))[^"]/g, '<a href="$1" target="_blank">$1</a>');
+    this.convertedText = this.convertedText.replace(/([^"])((?:http|ftp|https):\/\/(?:[\w+?\.\w+])+(?:[a-zA-Z0-9\~\!\@\#\$\%\^\&\*\(\)_\-\=\+\\\/\?\.\:\;\'\,]*))([^"])/g, '$1<a href="$2" target="_blank">$2</a>$3');
     this.convertedText = this.convertedText.replace(/\n/g, '<br>');
     this.widgetContent.nativeElement.innerHTML = this.convertedText;
   }
